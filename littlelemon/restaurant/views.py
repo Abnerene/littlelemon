aa80151c5ef0eabc9100d325a54fbf6f67375379
rene <abnerenergy@gmail.com>
2023-01-30 10:08:28 -0600

    creating models

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import models
 
+
+# Create your models here.
+class Booking(models.Model):
+   first_name = models.CharField(max_length=200)    
+   last_name = models.CharField(max_length=200)
+   guest_number = models.IntegerField()
+   comment = models.CharField(max_length=1000)
+
+   def __str__(self):
+      return self.first_name + ' ' + self.last_name
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+   name = models.CharField(max_length=200)
+   price = models.IntegerField(null=False)
+   menu_item_description = models.TextField(max_length =1000, default ='')
+
+
+
+   def __str__(self):
+      return self.name
 # Create your views here.
 def sayHello(request):
     return HttpResponse('Hello World')
@@ -8,3 +31,4 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+    
